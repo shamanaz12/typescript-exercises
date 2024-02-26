c9dcf831d8f1e5037109eb63e431c5471088afef
shamanaz12 <160182090+shamanaz12@users.noreply.github.com>
2024-02-26 22:09:16 +0500

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,30 @@
+//let personName :string = '';
+ let personName = prompt("what is your name?") || "";
+ if(personName !== null && personName !==""){
+     alert(`Hello ${personName}, would you like to learn some python today?`)
+ }
+ else{
+     alert(`you have to fil your name !`)
+ }
+    
+
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    ')
+
